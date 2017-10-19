d63524b0c8c6c2361f7b45512c30a16a464d7de7
Roberto Hernandez <roberto.hernandez@gruporaf.com>
2017-10-19 13:40:44 -0600

    react-bootstrap-style

--- src/components/CurrentDestination.js
+++ src/components/CurrentDestination.js
@@ -1,8 +1,9 @@
 import React, { Component } from 'react';
+import {Glyphicon, Form, FormControl,FormGroup,Grid,Col,Row,ControlLabel, Button, ButtonGroup  }from 'react-bootstrap';
 
 class CurrentDestination extends Component {
     render() {
-      return (
+     /* return (
         <div className="currentDestination">
             <h1>City, State</h1>
             <div>
@@ -13,6 +14,40 @@ class CurrentDestination extends Component {
             </div>
             <button>Flights from<span> &#36;000</span></button>
         </div>
+      );*/
+      return (
+        <Grid className="custom-grid">
+          <Row className="show-grid">
+            <Col sm={6} md={4}>
+            <div className="customH1">City, State</div>
+
+              <Form horizontal>
+
+                <FormGroup>
+                  <Col componentClass={ControlLabel} sm={2}>
+                  From
+                  </Col>
+                  <Col sm={10} >
+               
+                    <FormControl
+                      type="text"
+                      placeholder="Local Airport"
+                      bsSize="lg"
+                    />
+                  </Col>
+                </FormGroup>
+
+                <FormGroup className="buttons">
+                  <Col sm={6} md={2} >
+                    <Button type="submit">Flights From <strong>$56565656</strong></Button>
+                  </Col>
+                </FormGroup>
+              </Form>
+              
+            </Col>
+          </Row>
+       
+        </Grid>
       );
     }
   }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,29 +1,34 @@
 import React, { Component } from 'react';
-import {Nav, NavDropdown,Form, FormControl,FormGroup, Navbar,Button, ButtonGroup, NavbarHeader, NavbarInstance, MenuItem, NavItem }from 'react-bootstrap';
+import {Nav, NavDropdown, Glyphicon, Form, FormControl,FormGroup, Navbar,Button, ButtonGroup, NavbarHeader, NavbarInstance, MenuItem, NavItem }from 'react-bootstrap';
 
 class Header extends Component {
   render() {
     return (
+
       <Navbar>
           <Navbar.Header>
             <Navbar.Brand>
-              <a href="#">Home</a>
+             
+              <a href="in#">Github</a>
+             
             </Navbar.Brand>
             <Navbar.Toggle />
           </Navbar.Header>
-          <Navbar.Collapse>
-          <div className="center-search">
+        <Navbar.Collapse>
             <Navbar.Form >            
-              <FormGroup >                
-                  <FormControl type="text" placeholder="Search" />
+              <FormGroup  >                
+                  <FormControl  type="text"  placeholder="Search New Destination" />
               </FormGroup>
+              
               <Nav pullRight>
-              <NavItem eventKey={2} href="#">New Destination</NavItem>
+                <NavItem eventKey={2} href="#"><h3>New Destination <Glyphicon glyph="chevron-right"/></h3></NavItem>
               </Nav>
+           
             </Navbar.Form>
-            </div>
-          </Navbar.Collapse>
+            </Navbar.Collapse>
+    
       </Navbar>
+    
     );
   }
 }
